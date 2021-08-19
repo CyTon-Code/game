c57119e4f5427e53c0fb3654932eec449ea36db1
CyTon Code <cytoncode@gmail.com>
2021-08-19 16:32:46 +0000

    Add files via upload

---
+++ cache/test/test_my_classes.cpp
@@ -0,0 +1,47 @@
+/*
+ * File: test_my_classes.cpp
+ * Language: Cpp
+ * Author: Dal Bo Yob
+ * We: DBY Company
+ *
+ * Copyright 2021 DBY Company <dal@bo.yob>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+
+#include "../../lib/game.hpp"
+
+// TEST:
+int main()
+{
+    system("echo   Welcome to the game!"); // goto add mesages
+    // Container();
+    Block();
+    Cell();
+    Essence();
+    // Functions();
+    Game();
+    Man();
+    Mouse();
+    NotTangible();
+    Object();
+    Slot();
+    Tangible();
+}
+
